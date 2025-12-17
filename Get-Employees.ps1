2c73687226f883fc6fdd0a546144c92ed519b72e
Taylor-Moss <84684154+Taylor-Moss@users.noreply.github.com>
2025-12-16 21:47:19 -0600

    Add files via upload

---
+++ Get-Employees.ps1
@@ -0,0 +1,17 @@
+﻿Import-Module ActiveDirectory
+
+#get all active employees and count them > send to csv file
+$emp = Get-AdUser -Filter {(Enabled -eq $True)} |
+Where-Object {$_.DistinguishedName -like "*OU=Active Employees,OU=Corporate Employees,DC=example,DC=example"} |
+Select-Object UserPrincipalName | 
+Where-Object `
+{$_.UserPrincipalName -notlike 'value*' `
+-and $_.UserPrincipalName -notlike 'value*' `
+-and $_.UserPrincipalName -notlike 'value*' `
+-and $_.UserPrincipalName -notlike '*value*' `
+-and $_.UserPrincipalName -notlike '*value*' `
+-and $_.UserPrincipalName -notlike '*value*'} |
+Sort-Object UserPrincipalName
+$emp
+$emp.Count
+$emp | Export-Csv C:\IT\Employees.csv
